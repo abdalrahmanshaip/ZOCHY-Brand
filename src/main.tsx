4b68a508418b3009087d5c4c73e95efd644deb25
abdalrahman <abdalrahmanshaip@gmail.com>
2024-08-06 08:22:49 +0300

    edit auth with kinde to handle when page refresh the data not abel to lose

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <KindeProvider
-        clientId='b2e2e0b9e3f244638a435771e7e0b8ea'
+        clientId='75563ce8d6674be8a452f8219e4aed89'
         domain='https://localbrand.kinde.com'
         redirectUri='https://zochy-brand.vercel.app/'
         logoutUri='https://zochy-brand.vercel.app/'
